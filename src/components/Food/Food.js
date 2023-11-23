ca3951c7c25a30854a0396653b9b8e920b1e7c3f
Titus Sid <titus.sid91@gmail.com>
2023-11-23 17:13:24 +1300

    Food component update

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -3,23 +3,33 @@ import ReactDOM from 'react-dom'
 import './Food.css'
 import foodData from './FoodData'
 
-function FoodList() {
+function FoodPage() {
+  return (
+    <div className="foodPage">
+      <h1>List of best Food in town</h1>
+      <FoodList />
+    </div>
+  )
+}
+const FoodList = () => {
   const result = []
 
   foodData.map((food, index) => {
+    const { imageSrc, name, address, city, category, rating, reviewCount, id } =
+      food
     result.push(
-      <article className="food" id={index}>
-        <img src={food.imageSrc} alt={food.name} />
-        <h2>{food.name}</h2>
-        <p>{food.address}</p>
-        <p>{food.city}</p>
-        <h3>{food.category}</h3>
+      <article className="food" id={id}>
+        <img src={imageSrc} alt={name} />
+        <h2>{name}</h2>
+        <p>{address}</p>
+        <p>{city}</p>
+        <h3>{category}</h3>
         <h3 className="rating-bg"></h3>
-        <p>{food.reviewCount} reviews</p>
+        <p>{reviewCount} reviews</p>
       </article>
     )
   })
   return <section className="foodList">{result}</section>
 }
 
-export default FoodList
+export default FoodPage
