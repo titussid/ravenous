285d36ab5dfc465467a5e91a5f08060e43cef6c7
Titus Sid <titus.sid91@gmail.com>
2023-11-24 18:45:24 +1300

    remove all components

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './components/App/App'
-import Food from './components/Food/Food'
-import Book from './components/Book/Book'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-    <Book />
-    {/* <Food /> */}
   </React.StrictMode>
 )
