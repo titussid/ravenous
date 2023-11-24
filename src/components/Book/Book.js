c444d5d443477b865c53415efd2a8507e9c77456
Titus Sid <titus.sid91@gmail.com>
2023-11-24 18:46:42 +1300

    Event handler added

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -7,26 +7,54 @@ function BookPage() {
   return (
     <div className="bookPage">
       <h1>List of best selling books</h1>
+      <BookSearchEvents />
       <BookList />
     </div>
   )
 }
+const BookSearchEvents = () => {
+  const handleBookSearch = () => {
+    window.open(
+      'https://www.amazon.com/s?k=' +
+        document.getElementById('search').value.replace(/ /g, '+')
+    )
+  }
+  return (
+    <section>
+      <form action="" target="blank">
+        <h2 style={{ display: 'inline', marginRight: '15px' }}>Search Books</h2>
+        <input type="text" name="name" id="search" />
+        <button type="button" onClick={handleBookSearch}>
+          Search
+        </button>
+      </form>
+    </section>
+  )
+}
 
 const BookList = () => {
-  const result = []
+  const getBook = (id) => {
+    const book = bookArray.find((book) => book.id === id)
+    console.log(book)
+  }
 
   return (
     <div className="bookList">
       {bookArray.map((book) => {
-        // const { img, title, author, price, id } = book
-        return <Book {...book} key={book.id} />
+        return <Book {...book} key={book.id} getBook={getBook} />
       })}
     </div>
   )
 }
 
-const Book = ({ img, title, author, price, id }) => {
-  console.log({ img, title, author, price, id })
+const Book = ({ img, title, author, price, detail, id, getBook }) => {
+  // console.log({ img, title, author, price, id })
+  const displayTitle = () => {
+    console.log(title)
+  }
+  const getSinglebook = () => {
+    getBook(id)
+  }
   return (
     <article className="book">
       <img className="Image" src={img} alt={title} />
@@ -34,6 +62,13 @@ const Book = ({ img, title, author, price, id }) => {
       <h4 className="author"> {author}</h4>
       <p className="rating-bg"></p>
       <h3>price: {price}</h3>
+      <button type="button" onClick={getSinglebook}>
+        Fetch info
+      </button>
+      <p>For more info</p>
+      <form action={detail} target="blank">
+        <button type="submit">Click here</button>
+      </form>
     </article>
   )
 }
--- src/components/Book/BookData.js
+++ src/components/Book/BookData.js
@@ -5,6 +5,8 @@ const bookArray = [
     author: 'Matthew Perry',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81tdvyI0MeL._AC_UL600_SR600,400_.jpg',
     price: '60$',
+    detail:
+      'https://www.amazon.com/Friends-Lovers-Big-Terrible-Thing/dp/1250866448/ref=zg_bs_g_books_sccl_2/132-4067758-6664357?psc=1',
   },
   {
     id: 2,
@@ -12,6 +14,8 @@ const bookArray = [
     author: 'Rebecca Yarros',
     img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
     price: '54$',
+    detail:
+      'https://www.amazon.com/Fourth-Wing-Empyrean-Rebecca-Yarros/dp/1649374046/ref=zg_bs_g_books_sccl_1/132-4067758-6664357?psc=1',
   },
   {
     id: 3,
@@ -19,6 +23,8 @@ const bookArray = [
     author: 'Suzanne Collins',
     img: 'https://images-na.ssl-images-amazon.com/images/I/61k7JqSWOUL._AC_UL600_SR600,400_.jpg',
     price: '84$',
+    detail:
+      'https://www.amazon.com/Ballad-Songbirds-Snakes-Hunger-Games/dp/1339016575/ref=zg_bs_g_books_sccl_3/132-4067758-6664357?psc=1',
   },
 ]
 
